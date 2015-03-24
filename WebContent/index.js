bfcc9b7c0f3a0219604bf32db2573a48ae7e2609
Boris Charpiot <charpiot@de.ibm.com>
2015-03-24 08:42:54 -0500

    add tweet criteria area

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,93 +1,20 @@
 // index.js
 
-var REST_DATA = 'api/todolist';
 var REST_DBENV = 'api/dbinfo';
-var REST_TWITTERENV = 'api/twittterinfo';
+var REST_TWITTERENV = 'api/twitterinfo';
 var KEY_ENTER = 13;
 
-function loadItems(){
-	xhrGet(REST_DATA, function(data){
-		document.getElementById("loading").innerHTML = "";
-		var receivedItems = data.body || [];
-		var items = [];
-		var i;
-		// Make sure the received items have correct format
-		for(i = 0; i < receivedItems.length; ++i){
-			var item = receivedItems[i];
-			if(item && 'id' in item && 'name' in item){
-				items.push(item);
-			}
-		}
-		for(i = 0; i < items.length; ++i){
-			addItem(items[i], false);
-		}
-	}, function(err){
-		console.error(err);
-		document.getElementById("loading").innerHTML = "ERROR";
-	});
+function countTweets(){
+	// TOD
 }
 
-function addItem(item, isNew){
-	var row = document.createElement('tr');
-	var id = item && item.id;
-	if(id){
-		row.setAttribute('data-id', id);
-	}
-	row.innerHTML = "<td style='width:90%'><textarea onchange='saveChange(this)' onkeydown='onKey(event)'></textarea></td>" +
-		"<td class='deleteBtn' onclick='deleteItem(this)' title='delete me'></td>";
-	var table = document.getElementById('notes');
-	console.log(table.lastChild);
-	table.lastChild.appendChild(row);
-	var textarea = row.firstChild.firstChild;
-	if(item){
-		textarea.value = item.name;
-	}
-	row.isNew = !item || isNew;
-	textarea.focus();
-}
 
-function deleteItem(deleteBtnNode){
-	var row = deleteBtnNode.parentNode;
-	row.parentNode.removeChild(row);
-	xhrDelete(REST_DATA + '?id=' + row.getAttribute('data-id'), function(){
-	}, function(err){
-		console.error(err);
-	});
-}
-
-function onKey(evt){
-	if(evt.keyCode == KEY_ENTER && !evt.shiftKey){
-		evt.stopPropagation();
-		evt.preventDefault();
-		var row = evt.target.parentNode.parentNode;
-		if(row.nextSibling){
-			row.nextSibling.firstChild.firstChild.focus();
-		}else{
-			addItem();
-		}
-	}
-}
-
-function saveChange(contentNode, callback){
-	var row = contentNode.parentNode.parentNode;
-	var data = {
-		name: contentNode.value
-	};
-	if(row.isNew){
-		delete row.isNew;
-		xhrPost(REST_DATA, data, function(item){
-			row.setAttribute('data-id', item.id);
-			callback && callback();
-		}, function(err){
-			console.error(err);
-		});
+function toggleCount(contentNode){
+	var node = document.getElementById('counttips');
+	if (contentnode.toString().length() > 0){
+		node.style.display = '';		
 	}else{
-		data.id = row.getAttribute('data-id');
-		xhrPut(REST_DATA, data, function(){
-			console.log('updated: ', data);
-		}, function(err){
-			console.error(err);
-		});
+		node.style.display = 'none';
 	}
 }
 
@@ -133,5 +60,4 @@ function updateTwitterInfo(){
 }
 updateDatabaseInfo();
 updateTwitterInfo();
-loadItems();
 
